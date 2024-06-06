1003f8bd371c4368dc19e60660e7c78cf07f7e4d
Augusto Salazar <cesaraugustosalazar12345@gmail.com>
2024-06-06 08:20:34 -0500

    Template

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    my_set = {"Hello World, my name is: Augusto Salazar!"}
-    return jsonify(list(my_set))
+    render_template('index.html')
 
 
 if __name__ == '__main__':
